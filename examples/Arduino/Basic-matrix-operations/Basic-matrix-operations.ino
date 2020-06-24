299e61069c48adb5c0b86e062a1ae46840c9c720
Pieter Pas <pieter.pas@outlook.com>
2020-06-24 02:20:49 +0200

    Example docs

--- examples/Arduino/Basic-matrix-operations/Basic-matrix-operations.ino
+++ examples/Arduino/Basic-matrix-operations/Basic-matrix-operations.ino
@@ -1,3 +1,76 @@
+/**
+ * @example Basic-matrix-operations.ino
+ * @brief   Arduino sketch that demonstrates some of the basic features.
+ * 
+ * ---
+ * 
+ * Aimed at Arduino boards with STL ostream support. Tested on ESP32 and Teensy
+ * 4.0 (beta). Also compiles for Arduino SAMD (Zero) and Arduino Nano 33 BLE,
+ * but untested.
+ * 
+ * Expected output
+ * ---------------
+ * 
+ * ~~~
+ * Solve system Ax = v 
+ * ------------------- 
+ * 
+ * A = 
+ *               1              6              2
+ *               3              2              1
+ *               5              1              3
+ * 
+ * v = 
+ *               9
+ *               6
+ *               9
+ * 
+ * QR factorization of A: 
+ * Q = 
+ *       -0.169031       0.963676      -0.206779
+ *       -0.507093      0.0948696       0.856654
+ *       -0.845154      -0.249657      -0.472637
+ * R = 
+ *        -5.91608       -2.87352       -3.38062
+ *               0        5.72214        1.27325
+ *               0              0      -0.974814
+ * 
+ * solution x = 
+ *               1
+ *               1
+ *               1
+ * 
+ * Basic matrix operations 
+ * ----------------------- 
+ * 
+ *  C×B + D = 
+ *              32             39
+ *              61             77
+ * 
+ * Basic vector operations 
+ * ----------------------- 
+ * 
+ * a   =                    1              2              3
+ * 
+ * b   =                    4              6              5
+ * 
+ * a×b =                   -8              7             -2
+ * 
+ * a·b =      31
+ * 
+ * 
+ * Element access 
+ * -------------- 
+ * 
+ * A[2,0] = 5
+ * A[2,0] ← 100
+ * A = 
+ *               1              6              2
+ *               3              2              1
+ *             100              1              3
+ * ~~~
+ */
+
 #include <Linear_Algebra.h> // Include the library first
 
 #include <HouseholderQR.hpp> // Then include the necessary headers
